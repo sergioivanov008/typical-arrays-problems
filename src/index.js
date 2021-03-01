56331bcebc50160559d13db6f84a2b3ec6b6c538
sergioivanov008 <sergioivanov008@gmail.com>
2021-03-01 12:34:33 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (array == [] || array == [0]) {
+        return 0;
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    exports.max = function max(array) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    exports.avg = function avg(array) {
+        return 0;
+    }
