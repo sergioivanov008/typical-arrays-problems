29fe840a25871c113363e60a0266b15ac6d1150c
sergioivanov008 <sergioivanov008@gmail.com>
2021-03-01 13:42:30 +0300

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
 
 exports.min = function min(array) {
-    if (array == [] || array == [0]) {
+
+    if (array === undefined || array.length == 0) {
         return 0;
     }
 
-    exports.max = function max(array) {
+    let min = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        min = Math.min(min, array[i]);
+    }
+
+    return min;
+}
+
+exports.max = function max(array) {
+
+    if (array === undefined || array.length == 0) {
         return 0;
     }
 
-    exports.avg = function avg(array) {
+    let max = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        max = Math.max(max, array[i]);
+    }
+
+    return max;
+}
+
+exports.avg = function avg(array) {
+
+    if (array === undefined || array.length == 0) {
         return 0;
     }
+
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+
+
+    let avg = sum / array.length;
+
+    return avg;
+}
